b18da859fd3b06a10bb1947c5350e92f0594cd08
Thomas Wotoro <didymusthomas6@gmail.com>
2025-09-18 16:44:19 +0300

    Updated System for Return Book Function

--- Library/main.py
+++ Library/main.py
@@ -9,10 +9,20 @@ from Library_system import Users , Book , Library_system
 # print(user1)
 library = Library_system ()
 library.add_book("alice in wonderland")
-#library.add_book("Confess")
+library.add_book("Confess")
 print(library.Books)
 library.borrow("alice in wonderland")
 print(library.Borrowed_books)
-print(library.Books)
+print(library.Books) 
 
-    
+print("Returning Books.")
+library.return_book("alice in wonderland")
+if not library.Borrowed_books:
+    print("No borrowed Books available.")
+else:
+    print("Borrowed Books: ")
+    print(library.Borrowed_books)
+
+print("\nBooks available in the Library.")
+for index, books in enumerate(library.Books, 1):
+    print(f"\t{index}. {books}") 
